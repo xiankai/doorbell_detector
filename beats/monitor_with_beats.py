4b0ad15bd9269f60c155726cd2998c6df05afcde
Chia Kang Jin <xiankai@gmail.com>
2023-07-23 15:27:02 +0900

    convert to desired tensor format and resample using torchaudio.transforms

--- beats/monitor_with_beats.py
+++ beats/monitor_with_beats.py
@@ -1,6 +1,7 @@
 # Imports
 print('starting up (imports, initialization, etc.)')
 import torch
+from torchaudio import transforms
 from BEATs import BEATs, BEATsConfig
 import sounddevice as sd
 import numpy as np
@@ -28,6 +29,12 @@ BEATs_model.load_state_dict(checkpoint['model'])
 BEATs_model.eval()
 audio_array = np.zeros((1, int(desired_sample_rate)))
 
+# Define a function to resample audio
+def resample(tensor):
+  if recording_sample_rate != desired_sample_rate:
+    resampler = transforms.Resample(orig_freq=recording_sample_rate, new_freq=desired_sample_rate)
+    tensor = resampler(tensor)
+  return tensor
 
 # Inference loop
 stream = sd.InputStream(device = 0, channels = 1, samplerate=recording_sample_rate, blocksize = buffer_size)
@@ -37,8 +44,21 @@ with stream:
     # Record audio from the stream
     audio_array, _ = stream.read(buffer_size)
 
+    # Crude method of converting ndarray(15600,1) to Tensor.shape(3,10000) with copilot
 
-    probs = BEATs_model.extract_features(audio_input_16khz, padding_mask=padding_mask)[0]
+    # Reshape the audio_array to have shape (3, 5200, 1)
+    audio_array = audio_array.reshape(3, 5200, 1)
+    # Pad the audio_array along the second dimension to have length 10000
+    audio_array = np.pad(audio_array, ((0, 0), (0, 4800), (0, 0)), mode='constant')
+    # Remove the last dimension of the audio_array
+    audio_array = np.squeeze(audio_array)
+    # Convert the audio_array to a PyTorch tensor
+    tensor = torch.from_numpy(audio_array)
+
+    padding_mask = torch.zeros(3, 10000).bool()
+    # torch.no_grad is for performance https://github.com/microsoft/unilm/issues/998#issuecomment-1461310468
+    with torch.no_grad():
+      probs = BEATs_model.extract_features(tensor, padding_mask)[0]
 
     for i, (top5_label_prob, top5_label_idx) in enumerate(zip(*probs.topk(k=5))):
       top5_label = [checkpoint['label_dict'][label_idx.item()] for label_idx in top5_label_idx]
