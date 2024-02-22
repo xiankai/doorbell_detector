160e0d2d22310745b2a3afafd1017fa1e2351399
KJ <xiankai@gmail.com>
2024-02-23 00:57:12 +0900

    no need to turn on, can set brightness immediately + switch off after in case

--- notifications/tapo.py
+++ notifications/tapo.py
@@ -13,13 +13,14 @@ async def init():
   right_bulb = await client.l510(RIGHT_BULB)
   left_bulb = await client.l510(LEFT_BULB)
 
-  await right_bulb.on()
-  await left_bulb.on()
-
   return (right_bulb, left_bulb)
 
 async def flicker():
   (right_bulb, left_bulb) = await init()
+
+  device_info_json = await right_bulb.get_device_info_json()
+  if device_info_json['device_on'] == False:
+    was_off = True
 
   for i in range(1, 8):
     right_brightness = 80 if i % 2 == 0 else 20
@@ -29,8 +30,12 @@ async def flicker():
     time.sleep(1)
 
   print('resetting')
-  right_bulb.set_brightness(20)
-  left_bulb.set_brightness(20)
+  if was_off:
+    await right_bulb.off()
+    await left_bulb.off()
+  else:
+    right_bulb.set_brightness(20)
+    left_bulb.set_brightness(20)
 
 # For testing
 # import asyncio
