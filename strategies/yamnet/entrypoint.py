27a0d75f33715db683cf6c53d17da4387f86bd09
KJ <xiankai@gmail.com>
2024-02-25 17:41:19 +0900

    provisionally re-support yamnet (unable to build tflite-support@0.4 for apple silicon)

---
+++ strategies/yamnet/entrypoint.py
@@ -0,0 +1,28 @@
+import os
+from tflite_support.task import audio
+
+desired_sample_rate=16000 # specific to yamnet
+buffer_size=15600 # specific to yamnet
+audio_format=audio.AudioFormat(1,desired_sample_rate)
+
+classifier = None
+
+def init(model_path):
+  global classifier
+  classifier = audio.AudioClassifier.create_from_file(os.path.expanduser(model_path))
+
+def inference(tensor):
+  tensor_audio = audio.TensorAudio(audio_format=audio_format,buffer_size=buffer_size)
+  tensor_audio.load_from_array(tensor)
+  classification_result = classifier.classify(tensor_audio)
+
+  return format(classification_result)
+
+def format(classification_result):
+  formatted = []
+  for i in range(5):
+    formatted.append((
+      classification_result.categories[i].score,
+      classification_result.categories[i].category_name,
+    ))
+  return formatted
